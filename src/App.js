499a7618b098911d68a8e86a063f8032d462a604
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-05-08 16:27:12 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      GBP: '',
+      USD: '',
+      EUR: '',
+      hasConverted: false,
+      hasError: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.fetchRates = this.fetchRates.bind(this)
+    this.handleDisplay = this.handleDisplay.bind(this)
+  }
+
+  handleSubmit(e){
+    this.fetchRates();
+    e.preventDefault()
+  }
+
+  handleChange(e){
+    this.setState({GBP: e.target.value})
+  }
+
+  async fetchRates(){
+    let obj = {amount: this.state.GBP}
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+    try {
+      let res = await fetch(proxyurl + 'https://us-central1-ornate-apricot-220209.cloudfunctions.net/CodingTestFrontEnd-API?',
+        {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(obj)
+        })
+
+      let data = await res.json()
+
+      this.setState({
+        USD: data.USD.toFixed(2),
+        EUR: data.EUR.toFixed(2),
+        hasConverted: true
+      })
+
+    } catch(err) {
+      console.log(err)
+      this.setState({
+        hasConverted: false,
+        hasError: true
+      })
+    }
+  }
+
+  handleDisplay(){
+    return(
+      <div>
+        Your Money Will Buy You: 
+      <br/>
+        {
+          this.state.USD > this.state.EUR ? 
+            <div> USD {this.state.USD} <br/> EUR {this.state.EUR} <br/>That's {(this.state.USD - this.state.EUR).toFixed(2)} more USD than EUR!</div>  : 
+            <div> EUR {this.state.EUR} <br/> USD {this.state.USD} <br/>That's {(this.state.EUR - this.state.USD).toFixed(2)} more EUR than USD!</div>
+        } 
+      </div>
+    )    
+  }
+
+  render() {
+    console.log(this.state.USD)
+    return (
+      <div className='App'>
+
+        <div className='leftSide'>
+          <form id='GBPform' onSubmit={this.handleSubmit}>
+            Enter GBP Amount: 
+            <br/>
+            <input id='GBPinput' type='number' min='0' step='0.01' value={this.state.GBP} onChange={this.handleChange}></input>
+            <br/>
+            <input id='GBPsubmit' type='submit' value='Convert'></input>
+          </form>
+        </div>
+
+        <div className='rightSide'>
+          {this.state.hasConverted ? this.handleDisplay() : 
+            (this.state.hasError ? "There's Been A Problem. Refresh And Try Again In A Few Minutes" : '')}
+        </div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,92 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('render tests', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper).toMatchSnapshot()
+   });
+  
+  it('renders input form', () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.exists('#GBPform')).toEqual(true)
+  });
 });
+
+
+describe('input state changes', () => {
+
+  it('currency states are empty on launch', () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.state('GBP')).toEqual('')
+    expect(wrapper.state('EUR')).toEqual('')
+    expect(wrapper.state('USD')).toEqual('')
+  });
+
+  it('user input changes GBP state', () => {
+    const wrapper = shallow(<App/>)
+    const GBPinput = wrapper.find('#GBPinput')
+    const form = wrapper.find('#GBPform')
+
+    GBPinput.simulate('change', { target: { value: 14}})
+    form.simulate('submit', { preventDefault () {} })
+    expect(wrapper.state('GBP')).toBe(14)
+  });
+});
+
+describe('fetchRates', () => {
+  let mockEvent
+  let mockResponse
+  let mockfetchRates
+  let wrapper
+  
+  beforeEach( () =>{
+    mockEvent = { preventDefault: jest.fn() }
+    mockResponse = {USD: 18.00, EUR: 17.00};
+    mockfetchRates = jest.fn();
+    wrapper = shallow(<App fetchRates={mockfetchRates} />)
+  })
+
+  it('successful fetch changes state accordingly', done => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockResponse)
+    }))  
+    wrapper.instance().fetchRates(mockEvent)
+    .then(() => {
+      expect(wrapper.state('hasConverted')).toBeTruthy()
+      expect(wrapper.state('hasError')).toBeFalsy()
+      done()
+    });
+  });
+
+  it('unsuccessful fetch changes state accordingly', done => {
+    window.fetch = jest.fn().mockImplementationOnce(() => Promise.reject(
+      new Error('Fetch failed')
+    ))
+  
+    wrapper.instance().fetchRates(mockEvent)
+    .then(() => {
+      expect(wrapper.state('hasConverted')).toBeFalsy()
+      expect(wrapper.state('hasError')).toBeTruthy()
+      done()
+    });
+  })
+
+  it('fetch changes USD/EUR state', done => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockResponse)
+    })) 
+
+    wrapper.instance().fetchRates(mockEvent)
+    .then(() => {
+      expect(wrapper.state('USD')).toBe('18.00')
+      expect(wrapper.state('EUR')).toBe('17.00')
+      done()
+    })
+  });
+});
+    
+
+
